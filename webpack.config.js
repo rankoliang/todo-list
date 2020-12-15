636546034a49418e2fef791b8040c940bdc8f628
Ranko Liang <rankoliang@gmail.com>
2020-12-14 23:35:18 -0500

    Add styling without javascript

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "source-map",
+};
